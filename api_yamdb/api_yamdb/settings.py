cec5a1b192e1758377686dde72effd586a4f4578
ks-grshkv <kseniia.grshkv@yandex.ru>
2022-08-08 23:42:48 +0300

    Added users app, User has bio and role fields

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,10 +11,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
